a28f43a2b67a20539f39d09e695bf322901e9e35
Karthik D Nair <karthikdnair001@gmail.com>
2025-02-12 22:30:16 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body { margin: 0; padding: 10px; text-align: center; background: #333; color: white; font-size: 14px; }
+    </style>
+</head>
+<body>
+    <p>About, Contact, Site Owner Details</p>
+</body>
+</html>
