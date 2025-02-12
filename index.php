c3c4dfed7922543ed26761382323478d4161ab68
Karthik D Nair <karthikdnair001@gmail.com>
2025-02-12 19:34:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
-session_start();
+require 'db.php';
+
 if (!isset($_SESSION['logged_in'])) {
     header("Location: login.php");
     exit();
 }
-require 'db.php';
+
+$userCollection = $db->users;
+$user = $userCollection->findOne(['_id' => new MongoDB\BSON\ObjectId($_SESSION['user_id'])]);
+
 ?>
-<h1>Welcome to the Secure Website</h1>
+
+<h1>Welcome, <?= htmlspecialchars($user['name']) ?></h1>
+<a href="profile.php">Profile</a> |
 <a href="logout.php">Logout</a>
+
+<?php if ($user['role'] === 'admin'): ?>
+    <a href="admin_dashboard.php">Admin Dashboard</a>
+<?php else: ?>
+    <a href="user_dashboard.php">User Dashboard</a>
+<?php endif; ?>
