a94058c1c6eec5d658dca87bf4b517fca1e42fd9
Karthik D Nair <karthikdnair001@gmail.com>
2025-02-18 15:48:31 +0530

    Create edit_message.php

---
+++ edit_message.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require 'db.php';
+
+header("Content-Type: application/json");
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents("php://input"), true);
+    $messageId = $data["id"];
+    $newMessage = $data["message"];
+
+    $messageCollection = $db->messages;
+    $message = $messageCollection->findOne(['_id' => new MongoDB\BSON\ObjectId($messageId)]);
+
+    if ($message) {
+        if ($message['user_id'] == $_SESSION['user_id'] || $_SESSION['role'] === 'admin') {
+            $messageCollection->updateOne(
+                ['_id' => new MongoDB\BSON\ObjectId($messageId)],
+                ['$set' => ['message' => $newMessage]]
+            );
+            echo json_encode(["success" => true]);
+            exit();
+        }
+    }
+}
+
+echo json_encode(["success" => false]);
