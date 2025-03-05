b18e9e6a2c0b03b0cd879249335524d79e41dbff
Karthik D Nair <karthikdnair001@gmail.com>
2025-03-05 11:07:14 +0530

    Update main.php

--- main.php
+++ main.php
@@ -5,7 +5,7 @@
         body {
             font-family: Arial, sans-serif;
             padding: 20px;
-            background-image: url('https://www.google.com/url?sa=i&url=http%3A%2F%2Fmtithrissur.ac.in%2Fadmission-3%2F&psig=AOvVaw3P2M-ispI6oF-SvTY3a4XO&ust=1741238811767000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPDfv5ia8osDFQAAAAAdAAAAABAT');
+            background-image: url('unnamed.jpg');
             background-size: cover; /* This will cover the entire page with the background image */
             background-position: center; /* Center the image */
             background-attachment: fixed; /* Keeps the background image fixed while scrolling */
